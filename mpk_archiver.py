be7be063c3b40db152f97802eb1a144a4d33d18a
Piotr Babij <piotr.babij@gmail.com>
2017-09-30 16:43:55 +0200

    Writing in home dir...

--- mpk_archiver.py
+++ mpk_archiver.py
@@ -26,7 +26,7 @@ def main():
         ssh_key = os.getenv("ssh_key")
         if not ssh_key:
             raise Exception("'ssh_key' env variable must be specified!")
-        key_file, key_path = tempfile.mkstemp(dir="/opt/app-root/src")
+        key_file, key_path = tempfile.mkstemp(dir="/opt/app-root")
         with open(key_file) as f:
             f.write(ssh_key)
         os.chmod(key_path, 0o400)
